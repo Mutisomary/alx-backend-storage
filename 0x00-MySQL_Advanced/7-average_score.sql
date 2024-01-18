bbf2a0288dc4c50eb7ab20c8ef75107514a8bee0
Mutisomary <marymutso2016@gmail.com>
2024-01-18 07:53:33 +0000

    average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- store procedure ComputeAverageScoreForUser
+-- store the average score for a student
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INTEGER)
+BEGIN
+	UPDATE users
+	SET average_score = (SELECT avg(score)
+			     FROM corrections
+			     WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+END $$
+DELIMITER ;
