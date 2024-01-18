0f217fb20483b86cf811eab7a39c6740aa65f48d
Mutisomary <marymutso2016@gmail.com>
2024-01-18 06:40:41 +0000

    trigger to validate email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Assuming you have a table named 'user' with columns 'user_id', 'email', and 'valid_email'
+-- This trigger will reset 'valid_email' to 0 when 'email' is updated
+
+DELIMITER //
+CREATE TRIGGER trg_reset_valid_email
+BEFORE UPDATE ON user
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+DELIMITER ;
